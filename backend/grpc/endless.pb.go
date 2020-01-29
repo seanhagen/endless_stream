167fcc916accea1ff4b7af304c50b5681519d7bf
Sean Patrick Hagen <sean.hagen@gmail.com>
2020-01-28 18:30:36 -0800

    A bit more auto-generated code update

--- backend/grpc/endless.pb.go
+++ backend/grpc/endless.pb.go
@@ -7,8 +7,10 @@ import (
 	context "context"
 	fmt "fmt"
 	proto "github.com/golang/protobuf/proto"
-	_ "google.golang.org//genproto/googleapis/api/annotations"
+	_ "google.golang.org/genproto/googleapis/api/annotations"
 	grpc "google.golang.org/grpc"
+	codes "google.golang.org/grpc/codes"
+	status "google.golang.org/grpc/status"
 	math "math"
 )
 
@@ -23,65 +25,102 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
 
-type Updates struct {
-	Id                   string   `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
+type CreateGame struct {
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
 }
 
-func (m *Updates) Reset()         { *m = Updates{} }
-func (m *Updates) String() string { return proto.CompactTextString(m) }
-func (*Updates) ProtoMessage()    {}
-func (*Updates) Descriptor() ([]byte, []int) {
+func (m *CreateGame) Reset()         { *m = CreateGame{} }
+func (m *CreateGame) String() string { return proto.CompactTextString(m) }
+func (*CreateGame) ProtoMessage()    {}
+func (*CreateGame) Descriptor() ([]byte, []int) {
 	return fileDescriptor_602ff85d9c6c040c, []int{0}
 }
 
-func (m *Updates) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_Updates.Unmarshal(m, b)
+func (m *CreateGame) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_CreateGame.Unmarshal(m, b)
 }
-func (m *Updates) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_Updates.Marshal(b, m, deterministic)
+func (m *CreateGame) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_CreateGame.Marshal(b, m, deterministic)
 }
-func (m *Updates) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_Updates.Merge(m, src)
+func (m *CreateGame) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_CreateGame.Merge(m, src)
 }
-func (m *Updates) XXX_Size() int {
-	return xxx_messageInfo_Updates.Size(m)
+func (m *CreateGame) XXX_Size() int {
+	return xxx_messageInfo_CreateGame.Size(m)
 }
-func (m *Updates) XXX_DiscardUnknown() {
-	xxx_messageInfo_Updates.DiscardUnknown(m)
+func (m *CreateGame) XXX_DiscardUnknown() {
+	xxx_messageInfo_CreateGame.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_Updates proto.InternalMessageInfo
+var xxx_messageInfo_CreateGame proto.InternalMessageInfo
 
-func (m *Updates) GetId() string {
+type GameCreated struct {
+	Code                 string   `protobuf:"bytes,1,opt,name=code,proto3" json:"code,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *GameCreated) Reset()         { *m = GameCreated{} }
+func (m *GameCreated) String() string { return proto.CompactTextString(m) }
+func (*GameCreated) ProtoMessage()    {}
+func (*GameCreated) Descriptor() ([]byte, []int) {
+	return fileDescriptor_602ff85d9c6c040c, []int{1}
+}
+
+func (m *GameCreated) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_GameCreated.Unmarshal(m, b)
+}
+func (m *GameCreated) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_GameCreated.Marshal(b, m, deterministic)
+}
+func (m *GameCreated) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_GameCreated.Merge(m, src)
+}
+func (m *GameCreated) XXX_Size() int {
+	return xxx_messageInfo_GameCreated.Size(m)
+}
+func (m *GameCreated) XXX_DiscardUnknown() {
+	xxx_messageInfo_GameCreated.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_GameCreated proto.InternalMessageInfo
+
+func (m *GameCreated) GetCode() string {
 	if m != nil {
-		return m.Id
+		return m.Code
 	}
 	return ""
 }
 
 func init() {
-	proto.RegisterType((*Updates)(nil), "backend.grpc.Updates")
+	proto.RegisterType((*CreateGame)(nil), "endless.stream.v1.CreateGame")
+	proto.RegisterType((*GameCreated)(nil), "endless.stream.v1.GameCreated")
 }
 
 func init() { proto.RegisterFile("endless.proto", fileDescriptor_602ff85d9c6c040c) }
 
 var fileDescriptor_602ff85d9c6c040c = []byte{
-	// 189 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x6c, 0xce, 0x31, 0x6b, 0xc3, 0x30,
-	0x10, 0x86, 0xe1, 0xca, 0xd0, 0x96, 0x8a, 0xb6, 0x83, 0xa0, 0xd0, 0x9a, 0x0e, 0xc1, 0x93, 0x27,
-	0xc9, 0x38, 0x63, 0xb6, 0x2c, 0x21, 0x6b, 0x4c, 0x96, 0x2c, 0xe1, 0x6c, 0x1d, 0xb2, 0x88, 0x2d,
-	0x09, 0xe9, 0x92, 0xdf, 0x1f, 0x12, 0x34, 0x64, 0xc8, 0x7a, 0xdc, 0xf7, 0xf0, 0xf2, 0x2f, 0x74,
-	0x7a, 0xc2, 0x94, 0x64, 0x88, 0x9e, 0xbc, 0xf8, 0xec, 0x61, 0x38, 0xa1, 0xd3, 0xd2, 0xc4, 0x30,
-	0x94, 0xff, 0xc6, 0x7b, 0x33, 0xa1, 0x82, 0x60, 0x15, 0x38, 0xe7, 0x09, 0xc8, 0x7a, 0x97, 0x7f,
-	0xab, 0x3f, 0xfe, 0xbe, 0x0f, 0x1a, 0x08, 0x93, 0xf8, 0xe6, 0x85, 0xd5, 0xbf, 0x6c, 0xc1, 0xea,
-	0x8f, 0x5d, 0x61, 0x75, 0xbb, 0xe5, 0x7c, 0x03, 0x33, 0x76, 0x18, 0x2f, 0x18, 0xc5, 0x8a, 0xbf,
-	0x76, 0x04, 0x84, 0xe2, 0x47, 0x3e, 0xf2, 0x32, 0xaf, 0xcb, 0xe7, 0xe7, 0xea, 0xa5, 0x66, 0x0d,
-	0x5b, 0xb7, 0x87, 0xc6, 0x58, 0x1a, 0xcf, 0xbd, 0x1c, 0xfc, 0xac, 0x12, 0x82, 0x1b, 0xc1, 0xa0,
-	0x53, 0xb9, 0xfb, 0x98, 0x28, 0x22, 0xcc, 0x2a, 0x0b, 0xea, 0x26, 0xf4, 0x6f, 0xf7, 0xc0, 0xe5,
-	0x35, 0x00, 0x00, 0xff, 0xff, 0x3f, 0x96, 0x87, 0x5c, 0xdd, 0x00, 0x00, 0x00,
+	// 257 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0x4d, 0xcd, 0x4b, 0xc9,
+	0x49, 0x2d, 0x2e, 0xd6, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x12, 0x84, 0x71, 0x8b, 0x4b, 0x8a,
+	0x52, 0x13, 0x73, 0xf5, 0xca, 0x0c, 0xa5, 0x64, 0xd2, 0xf3, 0xf3, 0xd3, 0x73, 0x52, 0xf5, 0x13,
+	0x0b, 0x32, 0xf5, 0x13, 0xf3, 0xf2, 0xf2, 0x4b, 0x12, 0x4b, 0x32, 0xf3, 0xf3, 0xa0, 0x1a, 0xa4,
+	0xb8, 0x33, 0xf3, 0x0a, 0x4a, 0x4b, 0xa0, 0x1c, 0x9e, 0xfc, 0xd2, 0x12, 0x38, 0x4f, 0x89, 0x87,
+	0x8b, 0xcb, 0xb9, 0x28, 0x35, 0xb1, 0x24, 0xd5, 0x3d, 0x31, 0x37, 0x55, 0x49, 0x91, 0x8b, 0x1b,
+	0x44, 0x43, 0x44, 0x52, 0x84, 0x84, 0xb8, 0x58, 0x92, 0xf3, 0x53, 0x52, 0x25, 0x18, 0x15, 0x18,
+	0x35, 0x38, 0x83, 0xc0, 0x6c, 0xa3, 0xb5, 0x8c, 0x5c, 0x5c, 0x20, 0x35, 0xc1, 0xa9, 0x45, 0x65,
+	0xa9, 0x45, 0x42, 0xd1, 0x5c, 0x6c, 0x10, 0xd5, 0x42, 0xb2, 0x7a, 0x18, 0xce, 0xd2, 0x43, 0x18,
+	0x2d, 0x25, 0x87, 0x45, 0x1a, 0xc9, 0x2e, 0x25, 0xa1, 0xa6, 0xcb, 0x4f, 0x26, 0x33, 0xf1, 0x08,
+	0x71, 0xe9, 0x97, 0x19, 0xea, 0x27, 0x43, 0x8c, 0x74, 0xe2, 0x62, 0x0d, 0x2e, 0x01, 0x31, 0x24,
+	0xb0, 0x68, 0xf6, 0x04, 0xf9, 0x49, 0x4a, 0x12, 0x8b, 0x8c, 0x3f, 0xd8, 0x83, 0x4a, 0x0c, 0x1a,
+	0x8c, 0x06, 0x8c, 0x4e, 0x46, 0x51, 0x06, 0xe9, 0x99, 0x25, 0x19, 0xa5, 0x49, 0x7a, 0xc9, 0xf9,
+	0xb9, 0xfa, 0xc5, 0xa9, 0x89, 0x79, 0x19, 0x89, 0xe9, 0xa9, 0x79, 0xfa, 0x50, 0x6d, 0xf1, 0x10,
+	0x6d, 0xfa, 0x49, 0x89, 0xc9, 0xd9, 0xa9, 0x79, 0x29, 0xfa, 0xe9, 0x45, 0x05, 0xc9, 0x49, 0x6c,
+	0xe0, 0xb0, 0x31, 0x06, 0x04, 0x00, 0x00, 0xff, 0xff, 0x73, 0x5b, 0xce, 0x33, 0x78, 0x01, 0x00,
+	0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -96,6 +135,7 @@ const _ = grpc.SupportPackageIsVersion4
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type GameServerClient interface {
+	Create(ctx context.Context, in *CreateGame, opts ...grpc.CallOption) (*GameCreated, error)
 	State(ctx context.Context, opts ...grpc.CallOption) (GameServer_StateClient, error)
 }
 
@@ -107,8 +147,17 @@ func NewGameServerClient(cc *grpc.ClientConn) GameServerClient {
 	return &gameServerClient{cc}
 }
 
+func (c *gameServerClient) Create(ctx context.Context, in *CreateGame, opts ...grpc.CallOption) (*GameCreated, error) {
+	out := new(GameCreated)
+	err := c.cc.Invoke(ctx, "/endless.stream.v1.GameServer/Create", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 func (c *gameServerClient) State(ctx context.Context, opts ...grpc.CallOption) (GameServer_StateClient, error) {
-	stream, err := c.cc.NewStream(ctx, &_GameServer_serviceDesc.Streams[0], "/backend.grpc.GameServer/State", opts...)
+	stream, err := c.cc.NewStream(ctx, &_GameServer_serviceDesc.Streams[0], "/endless.stream.v1.GameServer/State", opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -117,8 +166,8 @@ func (c *gameServerClient) State(ctx context.Context, opts ...grpc.CallOption) (
 }
 
 type GameServer_StateClient interface {
-	Send(*Updates) error
-	Recv() (*Updates, error)
+	Send(*Input) error
+	Recv() (*Output, error)
 	grpc.ClientStream
 }
 
@@ -126,12 +175,12 @@ type gameServerStateClient struct {
 	grpc.ClientStream
 }
 
-func (x *gameServerStateClient) Send(m *Updates) error {
+func (x *gameServerStateClient) Send(m *Input) error {
 	return x.ClientStream.SendMsg(m)
 }
 
-func (x *gameServerStateClient) Recv() (*Updates, error) {
-	m := new(Updates)
+func (x *gameServerStateClient) Recv() (*Output, error) {
+	m := new(Output)
 	if err := x.ClientStream.RecvMsg(m); err != nil {
 		return nil, err
 	}
@@ -140,11 +189,41 @@ func (x *gameServerStateClient) Recv() (*Updates, error) {
 
 // GameServerServer is the server API for GameServer service.
 type GameServerServer interface {
+	Create(context.Context, *CreateGame) (*GameCreated, error)
 	State(GameServer_StateServer) error
+}
+
+// UnimplementedGameServerServer can be embedded to have forward compatible implementations.
+type UnimplementedGameServerServer struct {
+}
+
+func (*UnimplementedGameServerServer) Create(ctx context.Context, req *CreateGame) (*GameCreated, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Create not implemented")
+}
+func (*UnimplementedGameServerServer) State(srv GameServer_StateServer) error {
+	return status.Errorf(codes.Unimplemented, "method State not implemented")
 }
 
 func RegisterGameServerServer(s *grpc.Server, srv GameServerServer) {
 	s.RegisterService(&_GameServer_serviceDesc, srv)
+}
+
+func _GameServer_Create_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(CreateGame)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(GameServerServer).Create(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/endless.stream.v1.GameServer/Create",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(GameServerServer).Create(ctx, req.(*CreateGame))
+	}
+	return interceptor(ctx, in, info, handler)
 }
 
 func _GameServer_State_Handler(srv interface{}, stream grpc.ServerStream) error {
@@ -152,8 +231,8 @@ func _GameServer_State_Handler(srv interface{}, stream grpc.ServerStream) error 
 }
 
 type GameServer_StateServer interface {
-	Send(*Updates) error
-	Recv() (*Updates, error)
+	Send(*Output) error
+	Recv() (*Input, error)
 	grpc.ServerStream
 }
 
@@ -161,12 +240,12 @@ type gameServerStateServer struct {
 	grpc.ServerStream
 }
 
-func (x *gameServerStateServer) Send(m *Updates) error {
+func (x *gameServerStateServer) Send(m *Output) error {
 	return x.ServerStream.SendMsg(m)
 }
 
-func (x *gameServerStateServer) Recv() (*Updates, error) {
-	m := new(Updates)
+func (x *gameServerStateServer) Recv() (*Input, error) {
+	m := new(Input)
 	if err := x.ServerStream.RecvMsg(m); err != nil {
 		return nil, err
 	}
@@ -174,9 +253,14 @@ func (x *gameServerStateServer) Recv() (*Updates, error) {
 }
 
 var _GameServer_serviceDesc = grpc.ServiceDesc{
-	ServiceName: "backend.grpc.GameServer",
+	ServiceName: "endless.stream.v1.GameServer",
 	HandlerType: (*GameServerServer)(nil),
-	Methods:     []grpc.MethodDesc{},
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "Create",
+			Handler:    _GameServer_Create_Handler,
+		},
+	},
 	Streams: []grpc.StreamDesc{
 		{
 			StreamName:    "State",
