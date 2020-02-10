be8e0c2619668bc0005fd33b054761d12326cc3e
Sean Patrick Hagen <sean.hagen@gmail.com>
2020-02-10 00:50:42 -0800

    Trying to sort out this game state stuff

--- backend/game/state.go
+++ backend/game/state.go
@@ -20,29 +20,53 @@ type monster interface {
 
 type creature interface{}
 
+type actor interface {
+	hasScript() bool
+	runScript(*gameState) error
+	setNextAction(action)
+	action(*gameState) action
+}
+
+type action interface {
+	perform()
+}
+
 type gameState struct {
 	code string
 
-	current_initiative int
+	current_initiative_step int
+	current_initiative      int
+	max_initiative          int
 
 	players  map[string]player
 	audience map[string]audience
 	monsters map[string]monster
+
+	initiative map[int][]actor
 }
 
 func newState(ctx context.Context, id string) *gameState {
 	return &gameState{
-		code:               id,
-		current_initiative: 0,
-		players:            map[string]player{},
-		audience:           map[string]audience{},
-		monsters:           map[string]monster{},
+		code:                    id,
+		current_initiative_step: 0,
+		current_initiative:      0,
+		max_initiative:          0,
+		players:                 map[string]player{},
+		audience:                map[string]audience{},
+		monsters:                map[string]monster{},
 	}
 }
 
 // tick ...
 func (gs *gameState) tick(t time.Time) error {
-	// log.Printf("game state %v ticking onwards", gs.code)
+	// get current actor
+
+	//
+
+	// actor has action to perform?
+
+	// if no, return
+
 	return nil
 }
 
