0596a881e888fcc1dddef078d0a22108009d48de
Sean Patrick Hagen <sean.hagen@gmail.com>
2020-02-19 01:35:56 -0800

    blergh

--- player_browser/src/components/game.js
+++ player_browser/src/components/game.js
@@ -3,12 +3,16 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { Link, Route, withRouter } from "react-router-dom";
 import { v4 } from "uuid";
+import {grpc} from "@improbable-eng/grpc-web";
 
 import { gameState } from "../reducers/game";
-import { GameClient } from "../grpc/endless_pb_service";
+
+import { Game as GC } from "../grpc/endless_pb_service";
 import { ClientType, Input, Register } from "../grpc/input_pb";
 
-const gc = new GameClient(process.env.REACT_APP_GAME_SERVER);
+// import {BookService} from "../_proto/examplecom/library/book_service_pb_service";
+// import {QueryBooksRequest, Book, GetBookRequest} from "../_proto/examplecom/library/book_service_pb";
+//const gc = new GameClient(process.env.REACT_APP_GAME_SERVER);
 
 export class Game extends Component {
   constructor(props) {
@@ -19,10 +23,6 @@ export class Game extends Component {
       code: props.match.params.code
     };
     this.checkStream = this.checkStream.bind(this);
-  }
-
-  componentDidMount() {
-    // console.log("props: ", this.props.actions.connectToGame(this.state.code));
     const pid = v4();
     const reg = new Register();
     reg.setId(pid);
@@ -34,23 +34,38 @@ export class Game extends Component {
     const inp = new Input();
     inp.setRegister(reg);
 
-    const strm = gc.state();
-    strm
-      .on("data", msg => {
-        console.log("stream data: ", msg.toObject());
-        // dispatch(gameState.actions.receiveStreamMsg(msg.toObject()));
-      })
-      .on("status", msg => {
-        console.log("stream status: ", msg);
-      })
-      .write(inp);
+    const client = grpc.client(GC.State, {
+      host: process.env.REACT_APP_GAME_SERVER,
+    });
+    client.onHeaders((headers: grpc.Metadata) => {
+      console.log("queryBooks.onHeaders", headers);
+    });
+    client.onMessage((message: Book) => {
+      console.log("queryBooks.onMessage", message.toObject());
+    });
+    client.onEnd((code: grpc.Code, msg: string, trailers: grpc.Metadata) => {
+      console.log("queryBooks.onEnd", code, msg, trailers);
+    });
+    client.start();
+    client.send(inp);
 
-    this.stream = strm;
+    // const strm = gc.state();
+    // strm
+    //   .on("data", msg => {
+    //     console.log("stream data: ", msg.toObject());
+    //     // dispatch(gameState.actions.receiveStreamMsg(msg.toObject()));
+    //   })
+    //   .on("status", msg => {
+    //     console.log("stream status: ", msg);
+    //   })
+    //   .write(inp);
+
+    // this.stream = strm;
   }
 
   checkStream(ev) {
     ev.preventDefault();
-    console.log("stream client: ", this.stream.client);
+    console.log("stream client: ", this.stream);
   }
 
   render() {
