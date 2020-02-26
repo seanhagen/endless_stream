0480da6ba8804aeb3c34762d2c2e070720e1bd9b
Sean Patrick Hagen <sean.hagen@gmail.com>
2020-02-25 18:42:06 -0800

    Beefing up creatures
    Added ability to 'spawn' a creature from a base

--- backend/game/creature.go
+++ backend/game/creature.go
@@ -1,8 +1,13 @@
 package game
 
 import (
+	"bytes"
+	"log"
+
 	"github.com/seanhagen/endless_stream/backend/endless"
 	lua "github.com/yuin/gopher-lua"
+	"github.com/yuin/gopher-lua/parse"
+	luar "layeh.com/gopher-luar"
 )
 
 type creature struct {
@@ -39,10 +44,20 @@ type creature struct {
 	Script string
 
 	ls *lua.LState
+
+	proto *lua.FunctionProto
+
+	haveTick       bool
+	haveGetAction  bool
+	haveRound      bool
+	haveInitiative bool
+	luaFns         map[string]lua.LValue
 }
 
-// init ...
-func (c *creature) init() {
+// init should be called when first loading in the script. It creates a "base copy" of the creature,
+// with all the stats set up and the script read in. The lua State isn't set up yet though -- that's
+// what `spawn()` is for -- it copies the creature and calls `parse()`.
+func (c *creature) init() error {
 	c.Statuses = []Status{}
 	c.Modifiers = map[string]int32{}
 
@@ -60,4 +75,165 @@ func (c *creature) init() {
 	c.CombatDamageBase = statToMod[c.Strength]
 	c.VitalityRegen = statToMod[c.Strength]
 	c.Initiative = statToMod[c.Agility]
+
+	c.luaFns = map[string]lua.LValue{}
+
+	bits := bytes.NewBufferString(c.Script)
+	chunk, err := parse.Parse(bits, c.Name)
+	if err != nil {
+		return err
+	}
+	proto, err := lua.Compile(chunk, c.Name)
+	if err != nil {
+		return err
+	}
+	c.proto = proto
+
+	return nil
+}
+
+// spawn creates a 'live' copy of the creature, where the lua script has been initialized
+func (c *creature) spawn() (*creature, error) {
+	po := *c.Position
+	cr := &creature{
+		Id:          c.Id,
+		Name:        c.Name,
+		Description: c.Description,
+		Position:    &po,
+		Statuses:    []Status{},
+
+		Strength:        c.Strength,
+		MaxVitality:     c.MaxVitality,
+		CurrentVitality: c.CurrentVitality,
+		VitalityRegen:   c.VitalityRegen,
+
+		Intelligence: c.Intelligence,
+		CurrentFocus: c.CurrentFocus,
+		MaxFocus:     c.MaxFocus,
+		Willpower:    c.Willpower,
+		FocusRegen:   c.FocusRegen,
+
+		Agility:    c.Agility,
+		Evasion:    c.Evasion,
+		Accuracy:   c.Accuracy,
+		Initiative: c.Initiative,
+
+		Modifiers: map[string]int32{},
+		Gold:      c.Gold,
+		XP:        c.XP,
+		proto:     c.proto,
+		luaFns:    map[string]lua.LValue{},
+	}
+	err := cr.parse()
+	if err != nil {
+		return nil, err
+	}
+	return cr, nil
+}
+
+// parse uses the function proto to set up the lua state
+func (c *creature) parse() error {
+	//lua.NewState(opts ...lua.Options) *lua.LState
+	l := lua.NewState()
+	lfunc := l.NewFunctionFromProto(c.proto)
+	l.Push(lfunc)
+	if err := l.PCall(0, lua.MultRet, nil); err != nil {
+		return err
+	}
+	c.ls = l
+	if checkForFunction("tick", l) {
+		c.haveTick = true
+		c.luaFns["tick"] = l.GetGlobal("tick")
+	}
+
+	if checkForFunction("getAction", l) {
+		c.haveGetAction = true
+		c.luaFns["getAction"] = l.GetGlobal("getAction")
+	}
+
+	if checkForFunction("round", l) {
+		c.haveRound = true
+		c.luaFns["round"] = l.GetGlobal("round")
+	}
+
+	if checkForFunction("initiative", l) {
+		c.haveInitiative = true
+		c.luaFns["initiative"] = l.GetGlobal("initiative")
+	}
+
+	return nil
+}
+
+// checkForFunction ...
+func checkForFunction(name string, state *lua.LState) bool {
+	fn := state.GetGlobal(name)
+	return !lua.LVIsFalse(fn) && fn.Type() == lua.LTFunction
+}
+
+// tick ...
+func (c *creature) tick() error {
+	if c.haveTick {
+		c.ls.SetGlobal("creature", luar.New(c.ls, c))
+		call := lua.P{
+			Fn:      c.luaFns["tick"],
+			NRet:    1,
+			Protect: true,
+		}
+
+		if err := c.ls.CallByParam(call); err != nil {
+			return err
+		}
+
+		ret := c.ls.Get(-1)
+		c.ls.Pop(1)
+		log.Printf("creature tick output: %#v", ret)
+	}
+	return nil
+}
+
+// getAction ...
+func (c *creature) getAction(inp *endless.Input) (action, error) {
+	if c.haveGetAction {
+		c.ls.SetGlobal("creature", luar.New(c.ls, c))
+		call := lua.P{
+			Fn:      c.luaFns["getAction"],
+			NRet:    1,
+			Protect: true,
+		}
+
+		if err := c.ls.CallByParam(call, luar.New(c.ls, inp)); err != nil {
+			return nil, err
+		}
+
+		ret := c.ls.Get(-1)
+		c.ls.Pop(1)
+		log.Printf("creature getAction output: %#v", ret)
+	}
+	return nil, nil
+}
+
+// round ...
+func (c *creature) round() error {
+	return nil
+}
+
+// iniative ...
+func (c *creature) iniative() int {
+	if c.haveInitiative {
+		c.ls.SetGlobal("creature", luar.New(c.ls, c))
+		call := lua.P{
+			Fn:      c.luaFns["initiative"],
+			NRet:    1,
+			Protect: true,
+		}
+		if err := c.ls.CallByParam(call); err != nil {
+			return 0
+		}
+		ret := c.ls.Get(-1)
+		c.ls.Pop(1)
+		if i, ok := ret.(lua.LNumber); ok {
+			return int(i)
+		}
+	}
+	return 1
 }
--- backend/game/creature_test.go
+++ backend/game/creature_test.go
@@ -3,6 +3,8 @@ package game
 import (
 	"fmt"
 	"testing"
+
+	"github.com/seanhagen/endless_stream/backend/endless"
 )
 
 func TestCreatureInit(t *testing.T) {
@@ -55,5 +57,38 @@ func TestCreatureInit(t *testing.T) {
 
 		})
 	}
+}
 
+func TestCreatureInitiative(t *testing.T) {
+	tests := []struct {
+		name       string
+		script     string
+		expectInit int
+	}{
+		{"no initiative", "initBase = 0", 1},
+		{"simple intiative", `function initiative() return 3 end`, 3},
+		{"complex initiative", `function initiative() return 11 + creature.Initiative end`, 8},
+	}
+
+	for _, x := range tests {
+		tt := x
+		t.Run(fmt.Sprintf("test %v", tt.name), func(t *testing.T) {
+			p := endless.Position_Right
+			b := &creature{Script: tt.script, Position: &p}
+			err := b.init()
+			if err != nil {
+				t.Fatalf("unable to initialize creature: %v", err)
+			}
+
+			cr, err := b.spawn()
+			if err != nil {
+				t.Fatalf("unable to spawn active creature: %v", err)
+			}
+
+			init := cr.iniative()
+			if init != tt.expectInit {
+				t.Errorf("invalid value, expected '%v' got '%v'", tt.expectInit, init)
+			}
+		})
+	}
 }
