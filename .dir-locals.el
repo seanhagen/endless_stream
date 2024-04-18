bd128166bdccdd95bf6bd46cbe7b698f58e96483
Sean Patrick Hagen <sean.hagen@gmail.com>
2024-04-18 11:11:32 -0700

    Remove .dir-locals.el
    Only had stuff for golangci-lint-langserver, so not needed any more.

--- .dir-locals.el
+++
@@ -1,4 +0,0 @@
-;;; Directory Local Variables         -*- no-byte-compile: t; -*-
-;;; For more information see (info "(emacs) Directory Variables")
-
-((go-mode . ((lsp-golangci-lint-run-args . '("--skip-dirs-use-default" "--config" "/home/sean/Code/Go/src/github.com/seanhagen/endless_stream/.golangci.yml")))))
