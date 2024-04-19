e4d4bb18b9dbd0d96641eef1475f4539aaf25a0b
Sean Patrick Hagen <sean.hagen@gmail.com>
2024-04-19 09:23:06 -0700

    Fixing golangci-lint complaints

--- internal/transport/grpc/grpc.go
+++ internal/transport/grpc/grpc.go
@@ -1,3 +1,4 @@
+// Package grpc ...
 package grpc
 
 import (
@@ -6,29 +7,35 @@ import (
 	"github.com/seanhagen/endless_stream/internal/proto"
 )
 
+// HexServer ...
 type HexServer interface {
 	Info(context.Context, *proto.InfoRequest) (*proto.InfoResponse, error)
 	Game(proto.Hex_GameServer) error
 }
 
+// AdminServer ...
 type AdminServer interface {
 	Manage(proto.Admin_ManageServer) error
 }
 
+// ServerList ...
 type ServerList struct {
 	Hex   HexServer
 	Admin AdminServer
 }
 
+// Config ...
 type Config struct {
 	Servers ServerList
 }
 
+// Transport ...
 type Transport struct {
 	proto.UnimplementedHexServer
 	proto.UnimplementedAdminServer
 }
 
-func New(conf Config) (*Transport, error) {
+// New ...
+func New(_ Config) (*Transport, error) {
 	return &Transport{}, nil
 }
