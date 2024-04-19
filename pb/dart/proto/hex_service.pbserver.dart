0df6585f23402cd3c72313384e261ffbe0bc16fa
Sean Patrick Hagen <sean.hagen@gmail.com>
2024-04-19 09:18:46 -0700

    Pre-commit changes

--- pb/dart/proto/hex_service.pbserver.dart
+++ pb/dart/proto/hex_service.pbserver.dart
@@ -64,4 +64,3 @@ abstract class AdminServiceBase extends $pb.GeneratedService {
   $core.Map<$core.String, $core.dynamic> get $json => AdminServiceBase$json;
   $core.Map<$core.String, $core.Map<$core.String, $core.dynamic>> get $messageJson => AdminServiceBase$messageJson;
 }
-
